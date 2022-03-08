ba2f2c31e07dd4952a279faceb985c69765304c3
Aquiles Valerio Silva <aquilesvalerio@gmail.com>
2022-03-08 11:16:41 -0300

    Realizacao de testes v1

--- Ativacao.java
+++ Ativacao.java
@@ -1,5 +1,13 @@
 public class Ativacao {
     public static void main(String[] args) {
+        Conta contaAquiles = new Conta(12,"19653-6");
+        Conta contaTatiane = new Conta(13,"6547987465");
+
+        contaTatiane.deposita(5000);
+        contaTatiane.trasnfere(1000,contaAquiles);
+        System.out.println("Novo saldo é: R$ " + contaTatiane.getSaldo());
+        System.out.println("Novo saldo é: R$ "  + contaAquiles.getSaldo());
+
 
     }
 }
