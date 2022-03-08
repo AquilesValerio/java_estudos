7b62d0ae87d9d561bcccd50a860ed2578ba219b2
Aquiles Valerio Silva <aquilesvalerio@gmail.com>
2022-03-08 11:19:42 -0300

    Adicionado atributos e metodos

--- Endereco.java
+++ Endereco.java
@@ -1,2 +1,38 @@
 public class Endereco {
+    private String rua;
+    private String cep;
+    private String bairro;
+    private String UF;
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getUF() {
+        return UF;
+    }
+
+    public void setUF(String UF) {
+        this.UF = UF;
+    }
 }
