bf64ae3cdcbbd2de123e275da80ede04ef192cc7
Aquiles Valerio <99809205+AquilesValerio@users.noreply.github.com>
2022-03-08 11:24:13 -0300

    Correcao de cc e add contaBernardo

--- Ativacao.java
+++ Ativacao.java
@@ -1,7 +1,8 @@
 public class Ativacao {
     public static void main(String[] args) {
-        Conta contaAquiles = new Conta(12,"19653-6");
-        Conta contaTatiane = new Conta(13,"6547987465");
+        Conta contaAquiles = new Conta(12,"78903-6");
+        Conta contaTatiane = new Conta(13,"65479-8");
+        Conta contaBernardo = new Conta(14, "45689-7);
 
         contaTatiane.deposita(5000);
         contaTatiane.trasnfere(1000,contaAquiles);
