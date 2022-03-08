1f5e9561c62c5cbca85ae66fd3ef0294f7db50f0
Aquiles Valerio Silva <aquilesvalerio@gmail.com>
2022-03-08 11:18:07 -0300

    Adicionado metodos e a referencia do atributo endereco a classe Endereco

--- Cliente.java
+++ Cliente.java
@@ -1,5 +1,24 @@
 public class Cliente {
     private String nome;
     private String cpf;
-    private String endereco;
+    private Endereco endereco = new Endereco();
+
+    public String getNome (){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCpf(){
+        return this.cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+    public Endereco getEndereco(){
+        return this.endereco;
+    }
+    public void setEndereco(Endereco endereco){
+        this.endereco = endereco;
+    }
 }
